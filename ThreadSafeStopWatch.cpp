fecbe1e5574e2a1445383708de8fcb2621a10598
Ben.Aldrich <ben.aldrich@logrhythm.com>
2014-03-26 15:03:51 -0600

    fix race condition with thread container

--- ThreadSafeStopWatch.cpp
+++ ThreadSafeStopWatch.cpp
@@ -7,12 +7,12 @@ StopWatch& ThreadSafeStopWatch::GetStopWatch()
 {
    pthread_t tid { pthread_self() };
 
-   const auto it = mStopWatchMap.find(tid);
+   auto it = mStopWatchMap.find(tid);
    if (it == mStopWatchMap.end()) {
       std::lock_guard<std::mutex> lock(mMutex);
       // Lazy barbers lurk here, this should be safe
-      const auto findIt = mStopWatchMap.find(tid);
-      if (findIt == mStopWatchMap.end()) {
+      it = mStopWatchMap.find(tid);
+      if (it == mStopWatchMap.end()) {
          return mStopWatchMap[tid];
       }
    }

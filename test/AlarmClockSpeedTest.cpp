bd3bb7bc2fa240c8ea1bae43f252ac0dec1bc3cc
Amanda Carbonari <Amanda.Carbonari@logrhythm.com>
2016-01-06 09:09:20 -0700

    Added a speed test to see how much CPU time the reset takes

---
+++ test/AlarmClockSpeedTest.cpp
@@ -0,0 +1,34 @@
+/* 
+ * File:   AlarmClockSpeedTest.cpp
+ * Author: Amanda Carbonari
+ * Created: January 6, 2015 9:00am
+ *
+ * WARNING: This only measures CPU time
+ */
+
+#include <ctime>
+#include <iostream>
+#include <AlarmClock.h>
+using namespace std;
+typedef std::chrono::microseconds microseconds;
+typedef std::chrono::milliseconds milliseconds;
+typedef std::chrono::seconds seconds;
+
+
+template<typename T> void WaitForAlarmClockToExpire(AlarmClock<T>& alerter) {
+	while(!alerter.Expired());
+}
+
+int main(int, const char**) {
+ 	clock_t start;
+
+ 	AlarmClock<milliseconds> alerter(ms);
+
+ 	start = clock();
+ 	alerter.Reset()
+ 	clock_t duration = (clock() - start) / (double) (CLOCKS_PER_SEC/1000)
+
+ 	WaitForAlarmClockToExpire(alerter);
+
+ 	cout << "Time: " << duration << " ms" << endl;
+}

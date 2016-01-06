c025f5d85f6f861a03bc234ddb79e3c793ffef31
Amanda Carbonari <Amanda.Carbonari@logrhythm.com>
2016-01-06 10:50:39 -0700

    Changed timer to the boost auto cpu timer

--- AlarmClockSpeedTest.cpp
+++ AlarmClockSpeedTest.cpp
@@ -9,6 +9,7 @@
 #include <ctime>
 #include <iostream>
 #include <AlarmClock.h>
+#include <boost/timer/timer.hpp>
 using namespace std;
 typedef std::chrono::microseconds microseconds;
 typedef std::chrono::milliseconds milliseconds;
@@ -16,26 +17,29 @@ typedef std::chrono::seconds seconds;
 
 
 template<typename T> void WaitForAlarmClockToExpire(AlarmClock<T>& alerter) {
-	while(!alerter.Expired());
+   while(!alerter.Expired());
 }
 
 int main(int, const char**) {
- 	clock_t start;
- 	clock_t end;
- 	unsigned int us = 389;
+   clock_t start;
+   clock_t end;
+   unsigned int us = 389;
 
- 	cout << "Creating Alarm Clock" << endl;
- 	AlarmClock<microseconds> alerter(us);
- 	// Give some time for the countdown to start
- 	boost::this_thread::sleep_for(boost::chrono::microseconds(20));
- 	cout << "Starting clock and resetting" << endl;
- 	start = clock();
- 	alerter.Reset();
- 	end = clock();
- 	clock_t reset_time = (clock() - start) / (double) (CLOCKS_PER_SEC);
- 	cout << "Start: " << start << ", End: " << end << endl;
- 	cout << "Waiting for the clock to expire" << endl;
- 	WaitForAlarmClockToExpire(alerter);
+   cout << "Creating Alarm Clock" << endl;
+   AlarmClock<microseconds> alerter(us);
+   // Give some time for the countdown to start
+   boost::this_thread::sleep_for(boost::chrono::microseconds(20));
+   cout << "Starting clock and resetting" << endl;
+   //start = clock();
+   {
+      boost::timer::auto_cpu_timer t;
+      alerter.Reset();
+   }
+   //end = clock();
+   //clock_t reset_time = (clock() - start) / (double) (CLOCKS_PER_SEC);
+   //cout << "Start: " << start << ", End: " << end << endl;
+   cout << "Waiting for the clock to expire" << endl;
+   WaitForAlarmClockToExpire(alerter);
 
- 	cout << "Time: " << reset_time << " us" << endl;
+   //cout << "Time: " << reset_time << " us" << endl;
 }

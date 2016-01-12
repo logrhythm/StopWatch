17fac92e7e29f7144dfe9bd7b248a74d99112686
Amanda Carbonari <Amanda.Carbonari@logrhythm.com>
2016-01-12 10:02:07 -0700

    Added debugging statemetns

--- src/AlarmClock.h
+++ src/AlarmClock.h
@@ -36,7 +36,9 @@ public:
    }
 
    virtual ~AlarmClock() {
+      unique_lock<mutex> lck(mMutex);
       mExit.store(true);
+      mCondition.notify_all();
       StopBackgroundThread();
    }
    
@@ -45,15 +47,20 @@ public:
    }
 
    void Reset() {
+      cout << "RESET: Creating lock" << endl;
       unique_lock<mutex> lck(mMutex);
+      cout << "RESET: Setting mReset to true" << endl;
       mReset.store(true);
       // // If the thread isn't expired, stop it.
       // if (!mExpired.load()) {
       //    StopBackgroundThread();
       // }
       // Reset the expired value and notify the thread to restart
+      cout << "RESET: setting mExpired to 0" << endl;
       mExpired.store(0);
+      cout << "RESET: notifying all" << endl;
       mCondition.notify_all(); // Needed in the case it is already waiting
+      cout << "RESET: finished!" << endl;
    }
 
    // Used for performance testing, can be removed. 
@@ -73,28 +80,37 @@ protected:
 
    void AlarmClockInterruptableThread() {
       do {
+         cout << "THREAD: calling sleep function" << endl;
          // Call the sleep function
          unsigned int retVal = mSleepFunction(kSleepTimeUsCount);
 
          if (retVal == 0) {
+            cout << "THREAD: expired! " << (mExpired + 1) << endl;
             // Expired normally, should increment mExpired
             mExpired++;
          } else if (mExit) { // The thread was interrupted on a destructor
+            cout << "THREAD: break!" << endl;
             break;
          }
 
+         cout << "THREAD: Grabbing lock" << endl;
          unique_lock<mutex> lck(mMutex);
 
          if (!mReset) { // If the thread shouldn't reset
+            cout << "THREAD: Shouldn't reset, waiting on lock" << endl;
             // Wait to get notified. It will get notified under two conditions:
             //    1) Should restart
             //    2) Should exit
             // If it should exit, the while portion of the do while will execute,
             // if it should restart, it will automatically loop. 
             mCondition.wait(lck); 
+            cout << "THREAD: Done waiting on lock!" << endl;
          }
+         cout << "THREAD: setting reset to false because we are resetting" << endl;
          mReset.store(false);
+         cout << "THREAD: checking while loop" << endl;
       } while (!mExit);
+      cout << "THREAD: exiting!" << endl;
    }
   
    void StopBackgroundThread() {

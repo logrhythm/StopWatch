e66b9eca44b0cd8e2af503044579213fc0457b49
Amanda Carbonari <Amanda.Carbonari@logrhythm.com>
2016-01-15 08:35:02 -0700

    Removed cout statements and added more tests

--- AlarmClockSpeedTest.cpp
+++ AlarmClockSpeedTest.cpp
@@ -16,20 +16,40 @@ typedef std::chrono::milliseconds milliseconds;
 typedef std::chrono::seconds seconds;
 
 
-template<typename T> void WaitForAlarmClockToExpire(AlarmClock<T>& alerter) {
-   while(!alerter.Expired());
+template<typename T> int WaitForAlarmClockToExpire(AlarmClock<T>& alerter) {
+   int i = 1;
+   for(; !alerter.Expired(); ++i);
+   return i;
 }
 
-template <typename T> void testReset(unsigned int sleep_time, unsigned int reset_portion=2) {
-   // Start overall measurements
-   // cout << "Creating Alarm Clock" << endl;
+void getSleepOverhead(unsigned int sleep_time) {
    high_resolution_clock::time_point start_overall = high_resolution_clock::now();
-   AlarmClock<T> alerter(sleep_time);
-   // cout << "Starting clock and resetting" << endl;
+   AlarmClock<microseconds> alerter(sleep_time);
+   int forLoopVal = WaitForAlarmClockToExpire(alerter);
+   high_resolution_clock::time_point end_overall = high_resolution_clock::now();
+
+   auto overall_time = duration_cast<microseconds>(end_overall - start_overall).count();
+   int slept_time = alerter.SleepTimeUs();
+   auto overhead = overall_time - slept_time;
+   cout << "Results: " << endl;
+   cout << "\tOverall Time: " << overall_time << " us" << endl;
+   cout << "\tOverhead Time: " << overhead << " us" << endl;
+   cout << "\tSleep Time: " << slept_time << " us" << endl;
+   cout << "\tFor Loop Count: " << forLoopVal << endl;
+}
+
+void testReset(unsigned int sleep_time, int reset_portion=-1) {
+   // Start overall measurements
+   high_resolution_clock::time_point start_overall = high_resolution_clock::now();
+   AlarmClock<microseconds> alerter(sleep_time);
 
    // Sleep before restart, this can be changed to see the overhead for
    // different reset periods
-   std::this_thread::sleep_for(microseconds(sleep_time/reset_portion));
+   auto wait_amount = sleep_time/reset_portion;
+   if (reset_portion == -1) {
+      wait_amount = 0;
+   }
+   std::this_thread::sleep_for(microseconds(wait_amount));
 
    // Start the reset measurement
    high_resolution_clock::time_point start = high_resolution_clock::now();
@@ -42,7 +62,6 @@ template <typename T> void testReset(unsigned int sleep_time, unsigned int reset
    // Calculate the reset time
    auto reset_time = duration_cast<microseconds>(end - start).count();
    
-   // cout << "Waiting for the clock to expire" << endl;
    // Make sure the clock expires correctly after reset
    WaitForAlarmClockToExpire(alerter);
 
@@ -51,38 +70,51 @@ template <typename T> void testReset(unsigned int sleep_time, unsigned int reset
 
    // Calculate the overall time
    auto overall_time = duration_cast<microseconds>(end_overall - start_overall).count();
-   cout << "Results:" << endl;
-   cout << "\tOverall Time: " << overall_time << " us" << endl;
-   cout << "\tReset Time: " << reset_time << " us" << endl;
-   
-   // The overhead is calculated differently between Craig's and my code.
-   // Since I have code to time my sleep, I added a method that gets the 
-   // actual amount of sleep time. 
-   // Craig's code does not have this and I didn't want to change it's overall
-   // functionality to measure performance, so I just added the same method
-   // and have it return -1. If it does, then it just uses the amount of time
-   // the Alarm Clock should have slept for a measurement. 
    int slept_time = alerter.SleepTimeUs();
 
-   // Overhead is just the overall time minus the slept time. 
-   auto overhead = overall_time - slept_time;
+   // Overhead is just the overall time minus the slept time and wait period.
+   auto overhead = overall_time - slept_time - wait_amount;
+   cout << "Results:" << endl;
+   cout << "\tOverall Running Time: " << overall_time << " us" << endl;
    cout << "\tOverhead Time: " << overhead << " us" << endl;
+   cout << "\tOverhead Breakdowns: " << endl;
+   cout << "\t\tReset Time: " << reset_time << " us" << endl;
+   cout << "\t\tSleeping Overhead: " << duration_cast<microseconds>(end_overall - end).count() - slept_time << " us" << endl;
+   cout << "\tWait Before Restart Time: " << wait_amount << " us" << endl;
+   cout << "\tTime Spent Sleeping: " << slept_time << " us" << endl;
 }
 
 int main(int, const char**) {
    // The tester could take in different values as arguments, for now I have
    // some hard coded. 
-   unsigned int us = 400;
-   unsigned int ms = 3;
-   unsigned int s = 1;
+   microseconds us = microseconds(400);
+   milliseconds ms = milliseconds(3);
+   seconds s = seconds(1);
 
    for (int reset_time = 2; reset_time < 10; ++reset_time) {
       cout << "Reset @ 1/" << reset_time << endl;
-      cout << "---------------------------- Testing " << us << " microseconds ----------------------------" << endl;
-      testReset<microseconds>(us, reset_time);
-      cout << "---------------------------- Testing " << ms << " milliseconds ----------------------------" << endl;
-      testReset<milliseconds>(ms, reset_time);
-      cout << "---------------------------- Testing " << s << " seconds ----------------------------" << endl;
-      testReset<seconds>(s, reset_time);
+      cout << "---------------------------- Testing " << us.count() << " microseconds ----------------------------" << endl;
+      testReset(us.count(), reset_time);
+      cout << "---------------------------- Testing " << ms.count() << " milliseconds ----------------------------" << endl;
+      testReset(microseconds(ms).count(), reset_time);
+      cout << "---------------------------- Testing " << s.count() << " seconds ----------------------------" << endl;
+      testReset(microseconds(s).count(), reset_time);
    }
+
+   cout << "Reset Immediately" << endl;
+   cout << "---------------------------- Testing " << us.count() << " microseconds ----------------------------" << endl;
+   testReset(us.count());
+   cout << "---------------------------- Testing " << ms.count() << " milliseconds ----------------------------" << endl;
+   testReset(microseconds(ms).count());
+   cout << "---------------------------- Testing " << s.count() << " seconds ----------------------------" << endl;
+   testReset(microseconds(s).count());
+
+   cout << "No Reset and For Loop Test" << endl;
+   cout << "---------------------------- Testing " << us.count() << " microseconds ----------------------------" << endl;
+   getSleepOverhead(us.count());
+   cout << "---------------------------- Testing " << ms.count() << " milliseconds ----------------------------" << endl;
+   getSleepOverhead(microseconds(ms).count());
+   cout << "---------------------------- Testing " << s.count() << " seconds ----------------------------" << endl;
+   getSleepOverhead(microseconds(s).count());
+
 }
--- src/AlarmClock.h
+++ src/AlarmClock.h
@@ -2,18 +2,18 @@
  * File:   AlarmClock.h
  * Author: Amanda Carbonari
  * Created: January 5, 2016 4:35pm
- * Description: Utilizes the boost library to create a singleton interruptable thread
- *   that "communicates" with the main thread via a mutex, locks, and condition variables.
+ * Description: Uses an "interruptable" std::thread, that checks every
+ *    microsecond to see if any of the two interrupt atomics have been set to
+ *    true (mReset, mExit). If they are it will stop it's sleep and then check
+ *    to see if it was a reset or exit. If it was neither (expired) it will busy
+ *    wait until mReset is set to true and it can start sleeping again.  
  */
 
 #pragma once
 #include <chrono>
 #include <thread>
-#include <mutex>
 #include <atomic>
-#include <condition_variable>
 #include <functional>
-#include <iostream>
 #include "StopWatch.h"
 #include <cassert>
 using namespace std;
@@ -24,6 +24,7 @@ public:
    typedef chrono::milliseconds milliseconds;
    typedef chrono::seconds seconds;
 
+   // The sleep function is passed in for the unit tests. 
    AlarmClock(unsigned int sleepDuration, function<void (unsigned int)> funcPtr = nullptr) : mExpired(false),
       mSleptTime(0),
       mExit(false),
@@ -34,7 +35,10 @@ public:
       kSleepTimeUsCount(ConvertToMicrosecondsCount(Duration(sleepDuration))),
       mSleepFunction(funcPtr) {
          if (mSleepFunction == nullptr) {
-            mSleepFunction = bind(&AlarmClock::SleepUs, this, placeholders::_1);
+            mSleepFunction = [&](unsigned int sleepTime) 
+            { 
+               SleepUs(sleepTime); 
+            }; 
          }
          mTimerThread = thread(&AlarmClock::AlarmClockInterruptableThread, this);
    }
@@ -70,35 +74,28 @@ protected:
 
    void AlarmClockInterruptableThread() {
       do {
-         // Call the sleep function
-         // cout << "THREAD: Calling sleep function" << endl;
          mSleepFunction(kSleepTimeUsCount);
+         // This is required because the unit tests use their own "fake sleep"
+         // pass it into the constructor. By doing that, the unit tests will
+         // fail if the mExpired.store(true) is in the sleep function instead
+         // of the thread function. aka Due to unit test limitation.
          if (!mExit && !mReset) {
             mExpired.store(true);
          }
-         // cout << "THREAD: atomic values, mExit = " << mExit << ", mExpired = " << mExpired << ", mReset = " << mReset << endl;
-         if (mExit) { // The thread was interrupted on a destructor or 
-            // the destructor was called during the sleep function
-            // cout << "THREAD: mExit is " << mExit << " should exit." << endl;
+         if (mExit) {  
             break;
          }
 
          if (mExpired) {
-            // cout << "THREAD: mExpired is " << mExpired << " should wait for reset or exit." << endl;
             while (!mReset && !mExit) {
                this_thread::sleep_for(chrono::microseconds(1));
             }
-            // cout << "THREAD: Finished waiting for reset or exit" << endl;
          }
-         // cout << "THREAD: resetting reset value" << endl;
          mReset.store(false);
       } while (!mExit);
-      // cout << "THREAD: done! exiting" << endl;
    }
   
    void StopBackgroundThread() {
-      // Check to see if the thread is joinable and only join if it is supposed
-      // to exit.
       if (mTimerThread.joinable() && mExit) {
          mTimerThread.join();
       }
@@ -108,11 +105,11 @@ protected:
       StopWatch sw;
       for (int i = 1; i < t; ++i) {
          this_thread::sleep_for(chrono::microseconds(1));
+         auto elapsed = sw.ElapsedUs();
          if (mReset || mExit) {
             return;
          }
-         if (sw.ElapsedUs() >= t) {
-            mExpired.store(true);
+         if (elapsed >= t) {
             return;
          }
       }
@@ -142,6 +139,4 @@ private:
    const unsigned int kSleepTimeUsCount;
    function<void (unsigned int)> mSleepFunction;
    thread mTimerThread;
-   mutex mMutex;
-   condition_variable mCondition;
 };
